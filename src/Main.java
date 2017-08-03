fba2aa23887949206c1976aa21c24c6d96ef39a6
bzkxzol <30575362+bzkxzol@users.noreply.github.com>
2017-08-03 23:12:59 +0300

    Переписал чтоб можно было записывать в файл больше 1 строки

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,36 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Введите текст для записи в файл");
-        BufferedReader inpReader = new BufferedReader(new InputStreamReader(System.in));
-        String inputText = inpReader.readLine();
-        FileWriter(inputText);
-        System.console().readLine();
+        System.out.println ("Введите текст для записи в файл");
+        InputIntoFile();
     }
 
-    private static void FileWriter(String data){
-        File file = new File("InputtedData.txt");
-        FileWriter fr =null;
-        try{
-            fr = new FileWriter(file);
-            fr.write(data);
-        }catch (IOException e){
-            e.printStackTrace();
-        }finally {
-            try{
-                assert fr != null;
-                fr.close();
-            }catch (IOException e){
-                e.printStackTrace();
+    private static void InputIntoFile() {
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+        File file = new File ("InputtedData.txt");//файл запишется в ту же папку откуда запустили прогу
+        FileWriter fw = null;
+        String data;
+        try {
+            fw = new FileWriter (file , true);//флаг-true, для дозаписывания
+
+            //Записываем все что вводится с консоли, пока не будет какое-нибудь стоп-условие
+            while (true) {
+                data = br.readLine ();
+                if (!data.equals (""))// "" - можно заменить на какое нибудь ключевое слово типа Done(если нужно)
+                    fw.write (data + "\n");
+                else
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace ();
+        } finally {
+            try {
+                assert fw != null;
+                fw.close ();
+            } catch (IOException e) {
+                e.printStackTrace ();
             }
         }
-
-        System.out.println("\nТекст записан в файл:\n" + file.getAbsolutePath());
+        System.out.println ("\nТекст записан в файл:\n" + file.getAbsolutePath ());
     }
 }
